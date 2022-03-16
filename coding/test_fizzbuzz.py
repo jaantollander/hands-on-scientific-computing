d635e105bccc7838e282c4d3f7d1063c05fc10e4
Jaan Tollander de Balsch <jaan@hey.com>
2022-03-16 14:21:19 +0200

    test fizzbuzz

--- coding/test_fizzbuzz.py
+++ coding/test_fizzbuzz.py
@@ -1,3 +1,7 @@
 from fizzbuzz import fizzbuzz
+
 def test_fizzbuzz():
-    pass
+    assert fizzbuzz(3*5) == "fizzbuzz"
+    assert fizzbuzz(5) == "buzz"
+    assert fizzbuzz(3) == "fizz"
+    assert fizzbuzz(2) == 2
